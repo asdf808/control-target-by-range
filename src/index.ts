f96e97385a4c5103ad6a07f8059e15d33ce50bf4
Roy.Liu <Roy.Liu@fanruan.com>
2023-07-03 21:47:24 +0800

    feat: 滑块控制对象变化

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,21 @@
 const range = document.getElementById("range") as HTMLInputElement;
 const box = document.querySelector(".box") as HTMLDivElement;
+
+const maxTranslateX = range.clientWidth - box.clientWidth;
+
+range.value = "0";
+setBoxStyle(0);
+
+function setBoxStyle(value: number) {
+  const percent = value / 100;
+  box.style.cssText = `
+    border-radius: ${value / 2}%;
+    transform: translateX(${maxTranslateX * percent}px);
+    background: rgb(${255 * percent}, 255, 0)
+  `;
+}
+
+range.addEventListener("input", (e) => {
+  const target = e.target as HTMLInputElement;
+  setBoxStyle(target.valueAsNumber);
+});
